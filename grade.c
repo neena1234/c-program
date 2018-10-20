d5955ed586166f24ed1c2be8f0c276596cc5c213
neena1234 <neenanarayanan97@gmail.com>
2018-10-20 15:42:49 +0530

    Find the grade of students using elseif

---
+++ grade.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+char n;
+int m;
+printf("Enter the name of the student");
+scanf("%c",&n);
+printf("Enter the mark");
+scanf("%d",&m);
+if(p>=90)
+{
+printf("A+");
+}
+else if(:>=80&&m<=90)
+{
+printf("A");
+}
+else if(m>=70&&m<=80)
+{
+printf("B");
+}
+else if(m>=60&&m<=70)
+{
+printf("C");
+}
+else if(m>=50&&m<=60)
+{
+printf("D");
+}
+else if(m<=39)
+{
+printf("Fail");
+}
+}
+
+
