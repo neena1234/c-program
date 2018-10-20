4998cb078e3fc072b94ab4f1136cb1fa610b2b4c
neena1234 <neenanarayanan97@gmail.com>
2018-10-20 15:28:19 +0530

    amstrong numbers in a limit

---
+++ amstrong.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int 1,n,d,i;
+printf("enter the limit");
+scanf("%d",&1);
+printf("amstrong numbers are");
+i=1;
+while(i<=10
+{
+int p,s;
+n=i;
+s=0;
+while(n>0)
+{
+d=n%10;
+p=d*d*d;
+s=s+p;
+n=n/10;
+}
+if(s==i)
+{
+printf("%d",i);
+}
+i++;
+}
+}
