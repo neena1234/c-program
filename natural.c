0eb59fcb67008a41790c175331285dec009aa450
neena1234 <neenanarayanan97@gmail.com>
2018-10-20 15:37:12 +0530

    Natural numbersin a limit using dowhile

---
+++ natural.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0;
+i=l;
+printf("Enter the limit\n");
+scanf("%d",n);
+do
+{
+sum=sum+i;
+i++;
+
+while(i<=n)
+}
+printf("sum of first n natural numbers is %d",sum);
+}
+
