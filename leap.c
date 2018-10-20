c51f41cb9087cc6e35c61860e3a6529471200430
neena1234 <neenanarayanan97@gmail.com>
2018-10-20 15:39:58 +0530

    Check an year is leap or not

---
+++ leap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int x;
+printf("enter a year");
+scanf("%d",&x);
+if(x%4==0)
+{
+printf("The year is a leap year");
+}
+else
+{
+printf("The year is a not leap year");
+}
+}
+
+
