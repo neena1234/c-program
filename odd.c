c456b1cf0ff39225b7d9b1fbad12854406042841
neena1234 <neenanarayanan97@gmail.com>
2018-10-20 16:13:59 +0530

    check the number is odd or even

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,rem;
+printf("enter a number");
+scanf("%d",&a);
+rem=a%2;
+if(rem==0)
+{
+printf("The no is even/n");
+}
+if(rem!=0)
+{
+printf("the no is odd");
+}
+}
