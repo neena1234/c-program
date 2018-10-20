9182a21b84e867303f5cfd68243e2c0b4b33a6b7
neena1234 <neenanarayanan97@gmail.com>
2018-10-20 15:32:14 +0530

    Area of a triangle

---
+++ triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+int b,h,a;
+printf("enter the length of tiangle");
+scanf("%d",&b);
+printf("enter the length of hight triangle");
+scanf("%d",&h);
+a=0.5*b*h;
+printf("area of triangle is %d",&a);
+}
+
